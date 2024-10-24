8b44647310940ea884f013aed909160e3f662469
PrateekJha14 <139892883+PrateekJha14@users.noreply.github.com>
2024-10-24 14:06:24 +0530

    Initial commit for web3 todo list

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,106 @@
-import Image from "next/image";
+"use client";
+
+import { useRef } from "react";
+import { ABI } from "@/blockchain/abi";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useAccount, usePublicClient, useReadContract, useWriteContract } from "wagmi";
+
+const contractAddress = "0xDA0bab807633f07f013f94DD0E6A4F96F8742B53" as const;
+type Todo = {
+  id:bigint,
+  task: string,
+  isCompleted: boolean,
+  timestamp: bigint
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const inputRef = useRef<HTMLInputElement>(null);
+  const { address } = useAccount()
+  const publicClient = usePublicClient()
+  const {data, refetch} = useReadContract({
+    abi: ABI,
+    address: contractAddress,
+    functionName: 'getTodos',
+    args: [address],
+    query: {enabled: !!address}
+  })
+  
+  const userTodosCount = useReadContract({
+    abi: ABI,
+    address: contractAddress,
+    functionName: 'userTodoCount',
+    args: [address],
+    query: {enabled: !!address}
+  })
+  console.log('userTodosCount: ', userTodosCount.data);
+  console.log('todos: ', data);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+
+
+  const { writeContract} = useWriteContract({
+    mutation:{
+    onSuccess: async (data) => {
+      console.log('data: ', data)
+      await publicClient?.waitForTransactionReceipt({hash: data});
+      await refetch();
+      console.log('mutation success')
+    }
+  }})
+
+  const createTodo = () =>{
+    if(!inputRef.current && inputRef.current!.value) return console.error("No Input Value")
+      // console.log(createTodo);
+
+    writeContract({
+      abi: ABI,
+      address: contractAddress,
+      functionName: "createTodo",
+      args: [inputRef.current!.value]
+    })
+
+    inputRef.current!.value = "";
+  }
+
+  const deleteTodo = (id: bigint) => {
+    writeContract({
+      abi: ABI,
+      address: contractAddress,
+      functionName: "deleteTodo",
+      args: [id],
+    })
+  }
+
+  const toggleTodo = (id: bigint) => {
+    writeContract({
+      abi: ABI,
+      address: contractAddress,
+      functionName: "toggleTodo",
+      args: [id],
+    })
+  }
+
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <ConnectButton />
+
+      <div className="flex gap-5">
+        <Input ref={inputRef} />
+        <Button className="cursor-pointer active:scale-95" onClick={() => createTodo()}>Create Todo</Button>
+      </div>
+
+      <ul className="flex flex-col w-full max-w-[50rem]">
+        {data?.map((el: Todo) => (
+          <li key={el.id} className="flex gap-5 items-center border border-black rounded-lg p-2">
+            <p className="border rounded-lg px-2">{el.isCompleted ? "Done" : "Pending"}</p>
+            <p className="flex-1">{el.task}</p>
+            <Button className="cursor-pointer active:scale-95" onClick={() => deleteTodo(el.id)}>Delete</Button>
+            <Button className="cursor-pointer active:scale-95" onClick={() => toggleTodo(el.id)}>Toggle</Button>
+          </li>
+        ))}
+      </ul>
+    </main>
   );
 }
---
+++ context/Web3Provider.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultConfig,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import {
+  mainnet,
+  polygon,
+  optimism,
+  arbitrum,
+  base,
+  sepolia,
+} from 'wagmi/chains';
+import {
+  QueryClientProvider,
+  QueryClient,
+} from "@tanstack/react-query";
+import { ReactNode } from 'react';
+
+
+
+
+const config = getDefaultConfig({
+  appName: 'test',
+  projectId: 'cabf97cb91f45c44f9a565c779067a31',
+  chains: [mainnet, polygon, optimism, arbitrum, base, sepolia],
+  ssr: true, // If your dApp uses server side rendering (SSR)
+});
+
+const queryClient = new QueryClient();
+const Web3Provider = ({children}:{children: ReactNode}) => {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          {children}
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+};
+
+export default Web3Provider;
